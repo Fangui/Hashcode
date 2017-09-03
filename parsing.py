a06ea90cce950c24457e5fbaf1119114a8b8d1a0
Pleuvens Fervil <pleuvens.fervil@epita.fr>
2017-09-03 19:48:41 +0200

    parsing v2

--- parsing.py
+++ parsing.py
@@ -1,18 +1,30 @@
+import server.py
+import pool.py
+
 def readInput(path):
     f = open(path)
     nb_rows,nb_slots,nb_unavailable,nb_pools,nb_servers = [int(i) for i in f.readline().split(' ')]
     datacenter = []
+    
     for i in range(nb_rows):
         datacenter.append([0] * nb_slots)
+    
     unavailable = []
     servers = []
+    pools = []
+    
     for i in range(nb_unavailable):
         x,y = [int(i) for i in f.readline().split(' ')]
         unavailable.append((x,y))
         datacenter[x][y] = -1
+    
     for i in range(nb_servers):
         s,c = [int(i) for i in f.readline().split(' ')]
-        servers.append((s,c))
+        servers.append(server.server((-1,-1),s,c,1,i))
+    
+    for i in range(nb_pool):
+        pools.append(pool.pool([],float('inf'))
+
     print('rows: ' + str(nb_rows) + 'slots: ' + str(nb_slots))
     print(datacenter)
     print('nb unavailable: ' + str(nb_unavailable))
@@ -20,10 +32,4 @@ def readInput(path):
     print('nb servers: ' + str(nb_servers))
     print(servers)
     f.close()
-    return datacenter,unavailable,servers 
-
-def giveOutput(nb_servers, server):
-    for r,s,p in server:
-        print(str(r) + ' ' + str(s) + ' ' + str(p))
-    for i in range(nb_servers - len(server)):
-        print(x)
+    return datacenter,unavailable,servers,pools
