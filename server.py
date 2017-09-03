10311c4849bb49603279864e7266d640c0dd2ff4
Clement Fang <clement.fang@epita.fr>
2017-09-03 19:23:33 +0200

    server

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 class server:
-  def __init__(self, nbSlot, capacity):
-    self.nbSlot = nbSlot
+  def __init__(self, pos, size, capacity, free, ident):
+    self.pos = pos
+    self.size = size
     self.capacity = capacity
+    self.free = free
+    self.ident = ident
+
