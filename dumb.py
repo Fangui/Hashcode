91b567e50852807bca052aeca27857969d12404d
Pleuvens Fervil <pleuvens.fervil@epita.fr>
2018-03-01 22:09:26 +0100

    32M3

--- dumb.py
+++ dumb.py
@@ -1,12 +1,17 @@
 from parsing import *
 
+def distanceTP(a, b, c, d):
+  return abs(c - a) + abs(d - b)
+
 def dumb(path):
   rows, cols, vehicles, nb_rides, bonus, nb_steps, rides = readInput(path)
   output = []
   steps_array = []
+  vehicles_pos = []
   for i in range(vehicles):
     output.append([])
     steps_array.append(nb_steps)
+    vehicles_pos.append([0, 0, True, -1])
   ride_start = []
   distances = []
   count = 0
@@ -19,14 +24,19 @@ def dumb(path):
     if v >= vehicles:
       v = 0
     pos = ride_start.index(min(ride_start))
-    if count >= vehicles - 1:
-      ride_start[pos] = 1000000000000
-      count = 0
-    if distances[pos] > steps_array[v]:
-      v += 1
-      count += 1
-      continue
+    if count == 0:
+      dst = 100000000000
+      start_pos = [rides[pos][0], rides[pos][1]]
+      for j in range (len(vehicles_pos)):
+        if dst > distanceTP(start_pos[0], start_pos[1], vehicles_pos[j][0], vehicles_pos[j][1]):
+          v = j
+          dst = distanceTP(start_pos[0], start_pos[1], vehicles_pos[j][0], vehicles_pos[j][1])
     output[v].append(pos)
+    vehicles_pos[v][0] = rides[pos][2]
+    vehicles_pos[v][1] = rides[pos][3]
+    vehicles_pos[v][2] = False
+    vehicles_pos[v][3] = ride_start[pos]
+    vehicles
     ride_start[pos] = 1000000000000
     steps_array[v] -= distances[pos]
     count = 0
