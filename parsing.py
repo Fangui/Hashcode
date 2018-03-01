0a3a10079856b8cc40f6754f01c0de0cc4f6a9ac
Pleuvens Fervil <pleuvens.fervil@epita.fr>
2018-03-01 19:22:18 +0100

    [PARSING] added nb steps

--- parsing.py
+++ parsing.py
@@ -1,10 +1,13 @@
 def readInput(path):
-    f = open(path)
-    rows, cols, vehicles, nb_rides, bonus, nb_steps = [int(i) for i in f.readline().split(' ')]
-    rides = [];
-    
-    for i in range(nb_rides):
-        rides.append([int(i) for i in f.readline().split(' ')])
-    
-    f.close()
-    return rows, cols, vehicles, nb_rides, bonus, rides
+  f = open(path, "r")
+  rows, cols, vehicles, nb_rides, bonus, nb_steps = [int(i) for i in f.readline().split(' ')]
+  rides = [];
+
+  for i in range(nb_rides):
+      rides.append([int(i) for i in f.readline().split(' ')])
+
+  f.close()
+  return rows, cols, vehicles, nb_rides, bonus, nb_steps, rides
+
+#def createOutput(rides):
+#  f = open("toto", "w")
