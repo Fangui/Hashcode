d1c29637ae2c2cadac9e1f2c3fa986626d9a747a
Pleuvens Fervil <pleuvens.fervil@epita.fr>
2018-03-01 21:19:31 +0100

    [DUMB] dumb algo more opti

--- dumb.py
+++ dumb.py
@@ -3,19 +3,33 @@ from parsing import *
 def dumb(path):
   rows, cols, vehicles, nb_rides, bonus, nb_steps, rides = readInput(path)
   output = []
+  steps_array = []
   for i in range(vehicles):
     output.append([])
+    steps_array.append(nb_steps)
   ride_start = []
+  distances = []
+  count = 0
   for ride in rides:
     distance = abs(ride[2] - ride[0]) + abs(ride[3] - ride[1])
+    distances.append(distance)
     ride_start.append(ride[5])
   v = 0
   for i in range(nb_rides):
     if v >= vehicles:
       v = 0
     pos = ride_start.index(min(ride_start))
+    if count >= vehicles - 1:
+      ride_start[pos] = 1000000000000
+      count = 0
+    if distances[pos] > steps_array[v]:
+      v += 1
+      count += 1
+      continue
     output[v].append(pos)
     ride_start[pos] = 1000000000000
+    steps_array[v] -= distances[pos]
+    count = 0
     v += 1
   createOutput(path, output)
 
