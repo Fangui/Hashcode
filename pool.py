ab0c75e9db2bf54c4b84dc6e022343ce40d2f8ff
Damien Grisonnet <grison_d@epita.fr>
2018-03-01 18:25:25 +0100

    [CLEAN] clean repository

--- pool.py
+++
@@ -1,4 +0,0 @@
-class pool:
-  def __init__(self, servers, minCapacity):
-    self.servers = servers
-    self.minCapacity = minCapacity
--- server.py
+++
@@ -1,8 +0,0 @@
-class server:
-  def __init__(self, pos, size, capacity, free, ident):
-    self.pos = pos
-    self.size = size
-    self.capacity = capacity
-    self.free = free
-    self.ident = ident
-
